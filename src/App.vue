73d6aba113a02cd57489f82c3514916f72ab3848
jacky <jackywu526@gmail.com>
2023-09-05 10:04:46 +0800

    feat: install and import prettier

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
-  <el-card class="exam-card" v-for="(item, index) in dataContent.questions" :key="index">
-    <h2 class="exam-title">{{ `${index + 1}. ${item.question}` }}</h2>
-    <div class="exam-options">
-      <label class="exam-label" v-for="(option, optionIndex) in item.options" :key="optionIndex">
-        <input type="radio" :name="`question-${index}`" :value="option" v-model="item.userAnswer" :disabled="dataContent.showAnswer" :class="{ 'correct-answer': dataContent.showAnswer && option === item.answer && option === item.userAnswer, 'wrong-answer': dataContent.showAnswer && option === item.userAnswer && option !== item.answer }">
-        <span :class="{ 'correct-answer': dataContent.showAnswer && option === item.answer, 'wrong-answer': dataContent.showAnswer && option === item.userAnswer && option !== item.answer }">{{ option }}</span>
+  <el-card
+    class="exam-card max-w-screen-md mx-auto p-5 mb-5 w-80"
+    v-for="(item, index) in dataContent.questions"
+    :key="index"
+  >
+    <h2 class="exam-title text-2xl mb-4">
+      {{ `${index + 1}. ${item.question}` }}
+    </h2>
+    <div class="exam-options mb-[20px] flex flex-col">
+      <label
+        class="exam-label flex justify-center items-center"
+        v-for="(option, optionIndex) in item.options"
+        :key="optionIndex"
+      >
+        <input
+          type="radio"
+          :name="`question-${index}`"
+          :value="option"
+          v-model="item.userAnswer"
+          :disabled="dataContent.showAnswer"
+          class="mr-[10px]"
+          :class="{
+            'correct-answer':
+              dataContent.showAnswer &&
+              option === item.answer &&
+              option === item.userAnswer,
+            'wrong-answer':
+              dataContent.showAnswer &&
+              option === item.userAnswer &&
+              option !== item.answer,
+          }"
+        />
+        <span
+          :class="{
+            'correct-answer': dataContent.showAnswer && option === item.answer,
+            'wrong-answer':
+              dataContent.showAnswer &&
+              option === item.userAnswer &&
+              option !== item.answer,
+          }"
+          >{{ option }}</span
+        >
       </label>
     </div>
   </el-card>
@@ -12,61 +48,32 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const dataContent = ref({
   showAnswer: false,
   questions: [
     {
       question: "Question 1",
-      options: ['Option A', 'Option B', 'Option C', 'Option D'],
-      answer: 'Option A',
-      userAnswer: '',
+      options: ["Option A", "Option B", "Option C", "Option D"],
+      answer: "Option A",
+      userAnswer: "",
     },
     {
       question: "Question 2",
-      options: ['Option A', 'Option B', 'Option C', 'Option D'],
-      answer: 'Option A',
-      userAnswer: '',
+      options: ["Option A", "Option B", "Option C", "Option D"],
+      answer: "Option A",
+      userAnswer: "",
     },
-  ]
+  ],
 });
 
 const checkAnswers = () => {
   dataContent.value.showAnswer = true;
 };
-
 </script>
 
 <style scoped>
-.exam-card {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
-  margin-bottom: 20px;
-  width: 80%;
-}
-
-.exam-title {
-  font-size: 24px;
-  margin-bottom: 20px;
-}
-
-.exam-options {
-  margin-bottom: 20px;
-  display: flex;
-  flex-direction: column;
-}
-
-.exam-label {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.exam-label input {
-  margin-right: 10px;
-}
-
 .correct-answer {
   color: green !important;
 }
@@ -75,35 +82,35 @@ const checkAnswers = () => {
   color: red !important;
 }
 
-input[type='radio'].correct-answer:checked:after {
+input[type="radio"].correct-answer:checked:after {
   background-color: green;
 }
 
-input[type='radio'].wrong-answer:checked:after {
+input[type="radio"].wrong-answer:checked:after {
   background-color: red;
 }
 
-input[type='radio']:after {
+input[type="radio"]:after {
   width: 15px;
   height: 15px;
-  border-radius: 15px;
-  top: -2px;
+  border-radius: 50%;
+  top: -3px;
   left: -1px;
   position: relative;
-  content: '';
+  content: "";
   display: inline-block;
   visibility: visible;
   border: 2px solid white;
 }
 
-input[type='radio']:checked:after {
-  width: 13px;
-  height: 13px;
-  border-radius: 15px;
-  top: -2px;
+input[type="radio"]:checked:after {
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  top: -3px;
   left: -1px;
   position: relative;
-  content: '';
+  content: "";
   display: inline-block;
   visibility: visible;
   border: 2px solid white;
