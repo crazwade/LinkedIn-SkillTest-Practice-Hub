6b514d6708e38d086077d806ad4338f2e51774cd
jacky <jackywu526@gmail.com>
2023-09-05 09:47:44 +0800

    feat: install and import Tailwind css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
-import './style.css';
+import './style/style.css';
+import './style/tailwindCSS.css';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
